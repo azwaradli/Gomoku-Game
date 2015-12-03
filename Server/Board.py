1be38128e7ee8cbd9a04bf649defe14144238f00
azwaradli <azwar.adli@gmail.com>
2015-12-03 14:21:24 +0700

    board working correctly

--- Server/Board.py
+++ Server/Board.py
@@ -11,101 +11,103 @@ class Board(object):
 	def getBoard(self):
 		return self.arrayBoard
 
-	def placeBoard(self, x, y, pawn):
-		if self.isLegal(x, y):
-			self.arrayBoard[x][y] = pawn
+	def placeBoard(self, row, col, pawn):
+		if self.isLegal(row, col):
+			self.arrayBoard[row][col] = pawn
 			return True
 		else:
 			return False
 
-	def isLegal(self, x, y):
-		return self.arrayBoard[x][y] == LEGAL
+	def isLegal(self, row, col):
+		return self.arrayBoard[row][col] == LEGAL
 
 	def checkBoard(self, player):
 		# check if a player is winning
-		# by placing its pawn 5 in a row
+		# bcol placing its pawn 5 in a row
 		# STUB: still waiting for Game Logics
 		return True
 
-	def verticalChecking(self, x, y, pawn):
+	def verticalChecking(self, row, col, pawn):
 		count = 0
-		temp = x - 1
-		while temp >= 0 and self.arrayBoard[temp][y] == pawn and count < 5: # checking upper side
+		temp = row - 1
+		while temp >= 0 and self.arrayBoard[temp][col] == pawn and count < 4: # checking upper side
 			temp -= 1
 			count += 1
 
-		temp = x + 1
-		while temp < 20 and self.arrayBoard[temp][y] == pawn and count < 5: # checking lower side
+		temp = row + 1
+		while temp < 20 and self.arrayBoard[temp][col] == pawn and count < 4: # checking lower side
 			temp += 1
 			count += 1
 
-		if count >= 5:
+		if count >= 4:
 			return True
 		else:
 			return False
 
-	def horizontalChecking(self, x, y, pawn):
+	def horizontalChecking(self, row, col, pawn):
 		count = 0
-		temp = y - 1
-		while temp > 0 and self.arrayBoard[x][temp] == pawn and count < 5: # checking left side
+		temp = col - 1
+		while temp > 0 and self.arrayBoard[row][temp] == pawn and count < 4: # checking left side
+			print str(temp) + " upper"
 			temp -= 1
 			count += 1
 
-		temp = y + 1
-		while temp < 20 and self.arrayBoard[x][temp] == pawn and count < 5: # checking right side
+		temp = col + 1
+		while temp < 20 and self.arrayBoard[row][temp] == pawn and count < 4: # checking right side
+			print str(temp) + " lower"
 			temp += 1
 			count += 1
 
-		if count >= 5:
+		if count >= 4:
 			return True
 		else:
 			return False
 
-	def diagonalChecking(self, x, y, pawn):
+	def diagonalChecking(self, row, col, pawn):
 		count = 0
-		tempX = x + 1
-		tempY = y - 1
+		tempRow = row + 1
+		tempCol = col - 1
 		print "dig"
-		while (tempX < 20 and tempY >= 0) and self.arrayBoard[tempX][tempY] == pawn and count < 5:
+		while (tempRow < 20 and tempCol >= 0) and self.arrayBoard[tempRow][tempCol] == pawn and count < 4:
 			# checking quadran 1
-			tempX -= 1
-			tempY -= 1
+			tempRow -= 1
+			tempCol -= 1
 			count += 1
 
-		tempX = x - 1
-		tempY = y - 1
-		while (tempX >= 0 and tempY >= 0) and self.arrayBoard[tempX][tempY] == pawn and count < 5:
+		tempRow = row - 1
+		tempCol = col - 1
+		while (tempRow >= 0 and tempCol >= 0) and self.arrayBoard[tempRow][tempCol] == pawn and count < 4:
 			# checking quadran 2
-			tempX -= 1
-			tempY -= 1
+			tempRow -= 1
+			tempCol -= 1
 			count += 1
 
-		tempX = x - 1
-		tempY = y + 1
-		while (tempX >= 0 and tempY < 20) and self.arrayBoard[tempX][tempY] == pawn and count < 5:
+		tempRow = row - 1
+		tempCol = col + 1
+		while (tempRow >= 0 and tempCol < 20) and self.arrayBoard[tempRow][tempCol] == pawn and count < 4:
 			# checking quadran 3
-			tempX -= 1
-			tempY += 1
+			tempRow -= 1
+			tempCol += 1
 			count += 1
 
-		tempX = x + 1
-		tempY = y + 1
-		while (tempX < 20 and tempY < 20) and self.arrayBoard[tempX][tempY] == pawn and count < 5:
+		tempRow = row + 1
+		tempCol = col + 1
+		while (tempRow < 20 and tempCol < 20) and self.arrayBoard[tempRow][tempCol] == pawn and count < 4:
 			# checking quadran 4
-			tempX += 1
-			tempY += 1
+			tempRow += 1
+			tempCol += 1
 			count += 1
 
-		if count >= 5:
+		if count >= 4:
 			return True
 		else:
 			return False
 
 
-	def fiveRows(self, x, y, pawn):
+	def fiveRows(self, row, col, pawn):
 		# is this placement form a 5 or more pawn in a line?
 		
-		if self.verticalChecking(x, y, pawn) or self.horizontalChecking(x, y, pawn) or self.diagonalChecking(x, y, pawn):
+		if self.verticalChecking(row, col, pawn) or self.horizontalChecking(row, col, pawn) or self.diagonalChecking(row, col, pawn):
 			return True
 		else:
 			return False
@@ -116,7 +118,7 @@ def main():
 
 	while 1:
 
-		inp = raw_input('x y = ')
+		inp = raw_input('row col = ')
 		data = inp.split()
 
 		if board.placeBoard(int(data[0]), int(data[1]), 1):
@@ -125,7 +127,7 @@ def main():
 			if board.fiveRows(int(data[0]), int(data[1]), 1):
 				break
 	
-	print "you win"
+	print "win"
 
 if __name__ == '__main__':
 	main()
