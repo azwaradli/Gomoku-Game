dc129185da464c7c53f0bbe9ea24ae9e56907be4
azwaradli <azwar.adli@gmail.com>
2015-12-04 19:59:20 +0700

    fixing mistakes in Handler

--- Client/Handler.py
+++ Client/Handler.py
@@ -18,37 +18,37 @@ class Handler(object):
 						self.conn.close()
 						self.running = False
 
-				self.recv(self.msgRecv)
+				self.processDataRecv(self.msgRecv)
 
 	def processDataRecv(self, message):
 		if standard.MESSAGE not in message:
 			return
 
 		else:
-			if data[standard.MESSAGE] == standard.MESSAGE_AUTH:
-				if data[standard.MESSAGE_SUCCESS] == 1:
-					self.player_id = data[standard.PARAM_PLAYER_ID]
+			if message[standard.MESSAGE] == standard.MESSAGE_AUTH:
+				if message[standard.MESSAGE_SUCCESS] == 1:
+					self.player_id = message[standard.PARAM_PLAYER_ID]
 					print "login successful. your login key =", self.player_id
 				else:
 					print "login unsuccessful..."
 
-			elif data[standard.MESSAGE] == standard.MESSAGE_REFRESH:
-				for room in data["room_list"]:
+			elif message[standard.MESSAGE] == standard.MESSAGE_REFRESH:
+				for room in message["room_list"]:
 					print room
 
-			elif data[standard.MESSAGE] == standard.MESSAGE_CREATE_ROOM:
-				if data[standard.MESSAGE_SUCCESS] == 1:
+			elif message[standard.MESSAGE] == standard.MESSAGE_CREATE_ROOM:
+				if message[standard.MESSAGE_SUCCESS] == 1:
 					print "create room successful."
 				else:
 					print "create room unsuccessful..."
 
-			elif data[standard.MESSAGE] == "list_user":
-				for player in data["player_list"]:
+			elif message[standard.MESSAGE] == "list_user":
+				for player in message["player_list"]:
 					print player
 
-			elif data[standard.MESSAGE] == standard.MESSAGE_JOIN_ROOM:
-				if data[standard.MESSAGE_SUCCESS] == 1:
-					self.room_id = data[standard.PARAM_ROOM_ID]
+			elif message[standard.MESSAGE] == standard.MESSAGE_JOIN_ROOM:
+				if message[standard.MESSAGE_SUCCESS] == 1:
+					self.room_id = message[standard.PARAM_ROOM_ID]
 					print "you are connected to room", self.room_id
 				else:
 					print "login unsuccessful..."
